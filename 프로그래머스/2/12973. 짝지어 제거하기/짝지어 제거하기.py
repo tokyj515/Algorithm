65cbee7fa895ae9126ed6853eb8417e31b4f80f4
YuJin Kwon <tokyj515@naver.com>
2024-07-18 10:16:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 117.23 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(s):
+    alp = list(s)
+    stack = []
+    stack.append(alp[0])
+    alp.pop(0)
+    
+
+    for a in alp:
+        if stack and stack[-1] == a:
+            stack.pop()
+            
+        else:
+            stack.append(a)
+    
+    
+    if stack:
+        return 0
+    else:
+        return 1
+    
