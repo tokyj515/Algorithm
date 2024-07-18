96b3671ddeccd603bf6af0644bb43730b8b652c6
YuJin Kwon <tokyj515@naver.com>
2024-07-18 09:55:23 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,17 @@
+
+def solution(hp):
+    power = [5, 3, 1]
+    count = 0
+    #print(count)
+    
+
+    for p in power:
+        if hp >= p:
+            c, d = divmod(hp, p)
+            # print(c, d)
+            count += c
+            hp = d
+            
+    return count
+            
+    
