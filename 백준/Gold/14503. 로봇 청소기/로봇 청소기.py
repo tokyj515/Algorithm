1abb1cb999862afcab5b621c9d4b12d1f080cc84
YuJin Kwon <tokyj515@naver.com>
2024-10-04 23:19:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,59 +1,63 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, m = map(int, input().split(" "))
 
 graph = []
 
 # 시작 위치, 바라보는 방향
-start_x, start_y, d = map(int, input().split())
+start_x, start_y, d = map(int, input().split(" "))
 
-# 방향 설정: 북, 동, 남, 서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 result = 0
 
+
 for _ in range(n):
-    graph.append(list(map(int, input().split())))
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
 
 # 0: 청소 안 됨
 # 1: 벽
 # 2: 청소함
 
-def clean(x, y, d):
+
+def backtrack(x, y, d):
     global result
 
-    # 현재 위치를 청소
+
     if graph[x][y] == 0:
         graph[x][y] = 2
+        # print(x, y)
         result += 1
 
-    # 네 방향을 모두 확인
+
+
+    # 4방향을 기준으로 현재 가진 방향을 갖고 재귀
     for i in range(4):
-        # 왼쪽 방향으로 회전
-        d = (d + 3) % 4
+        d = (d+3)%4
+
         nx = x + dx[d]
         ny = y + dy[d]
 
-        # 그 방향으로 이동할 수 있으면 이동하고 재귀적으로 청소
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-            clean(nx, ny, d)
-            return
+        if 0<=nx and nx<n and 0<= ny and ny <m and graph[nx][ny] == 0:
+            backtrack(nx, ny, d)
+            return # 한 방향에서 재귀가 시작했으므로 다음 방향에서 실행되지 않도록 막는 것
 
-    # 네 방향 모두 청소가 되어 있거나 벽인 경우 후진
+
     bx = x - dx[d]
     by = y - dy[d]
 
-    # 후진할 수 있으면 후진
-    if 0 <= bx < n and 0 <= by < m and graph[bx][by] != 1:
-        clean(bx, by, d)
+    if 0 <= bx and bx < n and 0 <= by and by < m and graph[bx][by] != 1:
+        backtrack(bx, by, d)
     else:
-        # 후진도 불가능하면 멈춤
+        # 후진이 불가한 경우
         return
 
-# 청소 시작
-clean(start_x, start_y, d)
 
-# 결과 출력
+
+
+
+backtrack(start_x, start_y, d)
 print(result)
