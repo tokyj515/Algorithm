74b921eb5966167cd396a7c69ed59965367d4d9a
YuJin Kwon <tokyj515@naver.com>
2024-12-28 22:29:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.94 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,37 +1,45 @@
-from collections import deque
+from collections import deque, defaultdict
 
 
 def solution(n, edge):
-    graph = [[] for _ in range(n+1)]
-    visited = [0 for _ in range(n+1)]
-    depth = [0 for _ in range(n+1)]
-    queue = deque()
-    cnt = 0
-
-    # 그래프 세팅
-    for e in edge:
-        graph[e[0]].append(e[1])
-        graph[e[1]].append(e[0])
-
-    # bfs
-    queue.append(1)
-    visited[1] = 1
-
-    while queue:
-        v = queue.popleft()
-
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
-                depth[i] = depth[v] + 1
-
+    graph = defaultdict(list)
+    dep = [0 for _ in range(n+1)]
     
-    print(depth)
-                
-    max_depth = max(depth)
-    for i in depth:
-        if i == max_depth:
+    
+    for u, v in edge:
+        graph[u].append(v)
+        graph[v].append(u)
+        
+    
+    def bfs(start):
+        queue = deque()
+        visited = [0 for _ in range(n+1)]
+        
+        
+        queue.append(start)
+        visited[start] = 1
+        dep[start] = 1
+        
+        while queue:
+            v = queue.popleft()
+            
+            for next in graph[v]:
+                if not visited[next]:
+                    queue.append(next)
+                    visited[next] = 1
+                    dep[next] = dep[v]+1
+    
+    
+    bfs(1)
+    
+    max_val = max(dep)
+    
+    cnt = 0
+    for i in dep:
+        if i == max_val:
             cnt += 1
-
+            
+    
     return cnt
+        
+        
